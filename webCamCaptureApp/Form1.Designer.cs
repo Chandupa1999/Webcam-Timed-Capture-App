1d2e93d3d597f705ede88c598df7a6535eeb29c9
ychandupa1999 <yashasvic@icloud.com>
2025-04-04 23:31:21 +0530

    Capture images from web cam

---
+++ webCamCaptureApp/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace webCamCaptureApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            cboCamera = new ComboBox();
+            pic = new PictureBox();
+            btnStart = new Button();
+            panel1 = new Panel();
+            btnStop = new Button();
+            btnCaptureStart = new Button();
+            btnCaptureStop = new Button();
+            panel2 = new Panel();
+            label3 = new Label();
+            lblMessage = new Label();
+            panel3 = new Panel();
+            label4 = new Label();
+            btnViewImages = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            panel4 = new Panel();
+            label2 = new Label();
+            cboCaptureInterval = new ComboBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pic).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(120, 54);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Select Camera: ";
+            // 
+            // cboCamera
+            // 
+            cboCamera.FormattingEnabled = true;
+            cboCamera.Location = new Point(120, 80);
+            cboCamera.Name = "cboCamera";
+            cboCamera.Size = new Size(191, 23);
+            cboCamera.TabIndex = 1;
+            // 
+            // pic
+            // 
+            pic.BackgroundImage = (Image)resources.GetObject("pic.BackgroundImage");
+            pic.BackgroundImageLayout = ImageLayout.Zoom;
+            pic.BorderStyle = BorderStyle.FixedSingle;
+            pic.Location = new Point(98, 147);
+            pic.Name = "pic";
+            pic.Size = new Size(1120, 499);
+            pic.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic.TabIndex = 2;
+            pic.TabStop = false;
+            pic.Click += pic_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.ForeColor = Color.Navy;
+            btnStart.Location = new Point(231, 42);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(87, 25);
+            btnStart.TabIndex = 3;
+            btnStart.Text = "Connect";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click_1;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(btnStop);
+            panel1.Controls.Add(btnStart);
+            panel1.ForeColor = SystemColors.Highlight;
+            panel1.Location = new Point(98, 35);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(439, 81);
+            panel1.TabIndex = 4;
+            // 
+            // btnStop
+            // 
+            btnStop.ForeColor = SystemColors.Desktop;
+            btnStop.Location = new Point(331, 42);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(87, 25);
+            btnStop.TabIndex = 5;
+            btnStop.Text = "Disconnect";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnCaptureStart
+            // 
+            btnCaptureStart.Font = new Font("Segoe UI", 9F);
+            btnCaptureStart.ForeColor = Color.Chocolate;
+            btnCaptureStart.Location = new Point(831, 79);
+            btnCaptureStart.Name = "btnCaptureStart";
+            btnCaptureStart.Size = new Size(102, 23);
+            btnCaptureStart.TabIndex = 5;
+            btnCaptureStart.Text = "Start Capture";
+            btnCaptureStart.UseVisualStyleBackColor = true;
+            btnCaptureStart.Click += btnCaptureStart_Click;
+            // 
+            // btnCaptureStop
+            // 
+            btnCaptureStop.ForeColor = Color.Red;
+            btnCaptureStop.Location = new Point(130, 43);
+            btnCaptureStop.Name = "btnCaptureStop";
+            btnCaptureStop.Size = new Size(102, 23);
+            btnCaptureStop.TabIndex = 6;
+            btnCaptureStop.Text = "Stop Capture";
+            btnCaptureStop.UseVisualStyleBackColor = true;
+            btnCaptureStop.Click += btnCaptureStop_Click_1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(btnCaptureStop);
+            panel2.ForeColor = SystemColors.Highlight;
+            panel2.Location = new Point(814, 35);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 81);
+            panel2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.ForeColor = SystemColors.Desktop;
+            label3.Location = new Point(16, 18);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Capturing:";
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.BackColor = Color.Transparent;
+            lblMessage.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblMessage.ForeColor = Color.White;
+            lblMessage.Location = new Point(98, 123);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(17, 25);
+            lblMessage.TabIndex = 8;
+            lblMessage.Text = "l";
+            lblMessage.Visible = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Transparent;
+            panel3.BorderStyle = BorderStyle.FixedSingle;
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(btnViewImages);
+            panel3.ForeColor = SystemColors.Highlight;
+            panel3.Location = new Point(1090, 35);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(128, 81);
+            panel3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.ForeColor = SystemColors.Desktop;
+            label4.Location = new Point(18, 18);
+            label4.Name = "label4";
+            label4.Size = new Size(35, 15);
+            label4.TabIndex = 7;
+            label4.Text = "View:";
+            // 
+            // btnViewImages
+            // 
+            btnViewImages.ForeColor = Color.ForestGreen;
+            btnViewImages.Location = new Point(18, 43);
+            btnViewImages.Name = "btnViewImages";
+            btnViewImages.Size = new Size(92, 23);
+            btnViewImages.TabIndex = 6;
+            btnViewImages.Text = "View Captured";
+            btnViewImages.UseVisualStyleBackColor = true;
+            btnViewImages.Click += button2_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Transparent;
+            panel4.BorderStyle = BorderStyle.FixedSingle;
+            panel4.Controls.Add(label2);
+            panel4.Controls.Add(cboCaptureInterval);
+            panel4.ForeColor = SystemColors.Highlight;
+            panel4.Location = new Point(561, 35);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(229, 81);
+            panel4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.Desktop;
+            label2.Location = new Point(16, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Select Capture Interval:";
+            label2.Click += label2_Click;
+            // 
+            // cboCaptureInterval
+            // 
+            cboCaptureInterval.FormattingEnabled = true;
+            cboCaptureInterval.Location = new Point(16, 43);
+            cboCaptureInterval.Name = "cboCaptureInterval";
+            cboCaptureInterval.Size = new Size(191, 23);
+            cboCaptureInterval.TabIndex = 0;
+            cboCaptureInterval.SelectedIndexChanged += cboCaptureInterval_SelectedIndexChanged_1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.ForeColor = Color.Transparent;
+            button1.Location = new Point(1284, 9);
+            button1.Name = "button1";
+            button1.Size = new Size(30, 25);
+            button1.TabIndex = 9;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(1325, 684);
+            Controls.Add(button1);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(btnCaptureStart);
+            Controls.Add(pic);
+            Controls.Add(cboCamera);
+            Controls.Add(label1);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Controls.Add(lblMessage);
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "r";
+            FormClosing += Form1_FormClosing;
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)pic).EndInit();
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox cboCamera;
+        private PictureBox pic;
+        private Button btnStart;
+        private Panel panel1;
+        private Button btnStop;
+        private Button btnCaptureStart;
+        private Button btnCaptureStop;
+        private Panel panel2;
+        private Panel panel3;
+        private Button btnViewImages;
+        private System.Windows.Forms.Timer timer1;
+        private Label lblMessage;
+        private Panel panel4;
+        private ComboBox cboCaptureInterval;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Button button1;
+    }
+}
---
+++ webCamCaptureApp/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Drawing;
+using System.IO;
+using System.Timers;
+
+
+namespace webCamCaptureApp
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            captureTimer = new System.Windows.Forms.Timer();
+            captureTimer.Interval = 10000; // 10 seconds
+            captureTimer.Tick += CaptureTimer_Tick;
+
+            // Timer to hide the label after 3 seconds
+            messageTimer = new System.Windows.Forms.Timer();
+            messageTimer.Interval = 5000; // 3 seconds
+            messageTimer.Tick += MessageTimer_Tick;
+
+
+            // Ensure save directory exists
+            if (!Directory.Exists(savePath))
+            {
+                Directory.CreateDirectory(savePath);
+            }
+
+
+            // Initialize lblMessage properties (optional if set in Designer)
+            lblMessage.Visible = false;
+            lblMessage.BackColor = Color.Transparent;
+            lblMessage.ForeColor = Color.Black;
+            lblMessage.Font = new Font("Arial", 9, FontStyle.Bold);
+
+
+        }
+
+
+
+
+
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+        private System.Windows.Forms.Timer captureTimer; // Timer for auto-capturing
+        private System.Windows.Forms.Timer messageTimer; // Timer to clear the overlay message
+        private bool isCapturing = false; // Flag to track capturing state
+        private string savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "CapturedImages"); // Folder for saving images
+
+
+
+
+
+
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+
+            //btnStart.Enabled = false;
+            //btnStop.Enabled = true;
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice != null && videoCaptureDevice.IsRunning)
+            {
+                videoCaptureDevice.SignalToStop();
+                videoCaptureDevice.WaitForStop();
+                pic.Image = null;  // Clear the picture box after stopping
+            }
+            //btnStart.Enabled = true;
+            //btnStop.Enabled = false;
+        }
+
+
+
+
+
+
+
+
+        //image capturing part
+        private void btnCaptureStart_Click(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice == null || !videoCaptureDevice.IsRunning)
+            {
+                MessageBox.Show("Error: Please start the camera first!", "Camera Not Running", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+
+            if (isCapturing) return; // Prevent multiple messages
+
+            isCapturing = true;
+            captureTimer.Start(); // Start capturing every 10 seconds
+            ShowMessage("Capturing Started...");
+            MessageBox.Show("Images will be captured and stored from here...", "Start Capturing images", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Get the selected interval and apply it
+            captureTimer.Interval = GetIntervalFromSelection(cboCaptureInterval.SelectedItem.ToString());
+            captureTimer.Start();
+            // Disable the dropdown while capturing
+            cboCaptureInterval.Enabled = false;
+
+            // Disable Start button and enable Stop button
+           btnCaptureStart.Enabled = false;
+            btnCaptureStop.Enabled = true;
+        }
+
+        private void btnCaptureStop_Click_1(object sender, EventArgs e)
+        {
+            if (videoCaptureDevice == null || !videoCaptureDevice.IsRunning)
+            {
+                MessageBox.Show("Error: Please start the camera first!", "Camera Not Running", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            if (!isCapturing) return; // Prevent multiple messages
+
+            isCapturing = false;
+            captureTimer.Stop(); // Stop capturing
+            ShowMessage("Capturing Stopped");
+            MessageBox.Show("Capturing Images will be stoped from here...", "Stop Capturing images", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            // Enable the dropdown again when capturing stops
+            cboCaptureInterval.Enabled = true;
+
+            // Disable Stop button and enable Start button
+           btnCaptureStart.Enabled = true;
+           btnCaptureStop.Enabled = false;
+        }
+
+
+
+
+
+
+        private void btnViewImages_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", savePath); // Open folder
+        }
+
+        private void CaptureTimer_Tick(object sender, EventArgs e)
+        {
+            if (isCapturing && pic.Image != null)
+            {
+                CaptureImage();
+            }
+        }
+
+        private void CaptureImage()
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            string fileName = Path.Combine(savePath, $"Capture_{timestamp}.jpg");
+
+            try
+            {
+                pic.Image.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving image: " + ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+                cboCamera.Items.Add(filterInfo.Name);
+            cboCamera.SelectedIndex = 0;
+            videoCaptureDevice = new VideoCaptureDevice();
+
+
+
+            // Populate the capture interval dropdown
+            cboCaptureInterval.Items.AddRange(new object[]
+            {
+        "1 second","5 seconds", "10 seconds", "30 seconds", "1 minute",
+        "2 minutes", "3 minutes", "5 minutes", "10 minutes",
+        "15 minutes", "30 minutes", "1 hour", "2 hours",
+        "3 hours", "5 hours", "10 hours", "12 hours", "24 hours"
+            });
+
+            // Set default interval to 10 seconds
+            cboCaptureInterval.SelectedIndex = 1;
+
+            // Attach event to handle selection change
+            cboCaptureInterval.SelectedIndexChanged += cboCaptureInterval_SelectedIndexChanged;
+        }
+
+
+
+
+
+
+
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pic.Invoke((MethodInvoker)delegate
+            {
+                pic.Image = (Bitmap)eventArgs.Frame.Clone();
+            });
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoCaptureDevice.IsRunning == true)
+                videoCaptureDevice.Stop();
+        }
+
+
+
+
+
+
+
+
+        // Function to show a message in the label for 3 seconds
+        private void ShowMessage(string message)
+        {
+            lblMessage.Text = message;
+            lblMessage.Visible = true;
+            messageTimer.Start(); // Start the 3-second countdown
+        }
+
+        // Function to hide the message after 3 seconds
+        private void MessageTimer_Tick(object sender, EventArgs e)
+        {
+            lblMessage.Visible = false;
+            messageTimer.Stop();
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void cboCaptureInterval_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int interval = GetIntervalFromSelection(cboCaptureInterval.SelectedItem.ToString());
+            captureTimer.Interval = interval;
+        }
+
+        // Helper function to convert selected text to milliseconds
+        private int GetIntervalFromSelection(string selectedText)
+        {
+            switch (selectedText)
+            {
+                case "1 second": return 1000;
+                case "5 seconds": return 5000;
+                case "10 seconds": return 10000;
+                case "30 seconds": return 30000;
+                case "1 minute": return 60000;
+                case "2 minutes": return 120000;
+                case "3 minutes": return 180000;
+                case "5 minutes": return 300000;
+                case "10 minutes": return 600000;
+                case "15 minutes": return 900000;
+                case "30 minutes": return 1800000;
+                case "1 hour": return 3600000;
+                case "2 hours": return 7200000;
+                case "3 hours": return 10800000;
+                case "5 hours": return 18000000;
+                case "10 hours": return 36000000;
+                case "12 hours": return 43200000;
+                case "24 hours": return 86400000;
+                default: return 10000; // Default to 10 seconds if something goes wrong
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void pic_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+
+        private void btnCaptureStop_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboCaptureInterval_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string aboutText = "Camera Capturing System\n\n" +
+                       "Version: 1.0\n" +
+                       "Developed by: Chandupa Perera\n\n" +
+                       "This application allows users to capture images from a connected webcam at customizable time intervals. Users can start and stop capturing images, select a delay between captures, and access saved images.\n\n" +
+                       "Features:\n" +
+                       "✔ Displays a real-time preview of the selected camera.\n" +
+                       "✔ Allows users to start and stop automatic image capturing.\n" +
+                       "✔ Users can select time interval from 5 seconds to 24 hours.\n" +
+                       "✔ Captured images are saved with timestamp as the filename.\n" +
+                       "✔ A dedicated button to access and view saved images.\n\n" +
+                       "📧 Contact: yashasvichandupa@gmail.com\n" +
+                       "📞 Phone: +94713684090";
+
+
+            MessageBox.Show(aboutText, "Camera Capturing System", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+    }
+}
